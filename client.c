f3d20e2ee05785cd0e8b0416ff08d47ffc7803c1
ABDUL RAHMAN BIN ABD KADIR <32711980+rahman1996@users.noreply.github.com>
2017-12-02 07:31:09 -0800

    Update client.c

--- client.c
+++ client.c
@@ -4,57 +4,32 @@
 #include<arpa/inet.h> 
 #include<netinet/in.h>
  
-int main(int argc , char *argv[])
+int main(int argc,char **argv)
 {
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-     
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    server.sin_addr.s_addr = inet_addr("192.168.19.128");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+    int sockfd,n;
+    char sendline[100];
+    char recvline[100];
+    struct sockaddr_in servaddr;
  
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
-     
-    puts("Connected\n");
-     
-    //keep communicating with server
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    bzero(&servaddr,sizeof servaddr);
+ 
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(22000);
+ 
+    inet_pton(AF_INET,"192.168.19.128",&(servaddr.sin_addr));
+ 
+    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+ 
     while(1)
     {
-        printf("Enter message : ");
-        scanf("%s" , message);
-         
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-         
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-         
-        puts("Server reply :");
-        puts(server_reply);
+        bzero( sendline, 100);
+        bzero( recvline, 100);
+        fgets(sendline,100,stdin); /*stdin = 0 , for standard input */
+ 
+        write(sockfd,sendline,strlen(sendline)+1);
+        read(sockfd,recvline,100);
+        printf("%s",recvline);
     }
-     
-    close(sock);
-    return 0;
+ 
 }
